1be67fbb710fdf77307bd1706a68babcc42cead5
Xintong Ding <dxt@berkeley.edu>
2022-11-04 14:33:08 -0700

    hw5 submit

--- react-app/src/Components/Comments.js
+++ react-app/src/Components/Comments.js
@@ -2,17 +2,18 @@ import {useState} from "react";
 import axios from "axios";
 
 const Comments = ({ postId, comments: initialComments }) => {
-  const [_________, _________] = useState(initialComments);
+  const [comments, setComments] = useState(initialComments);
   const [newComment, setNewComment] = useState('');
 
   const handleSubmitComment = () => {
     console.log(newComment)
     // Un-comment the lines below to complete your solution
     // ====================
-    // axios.post(__________________, { newComment }).then((res) => {
-    //   ________________;
-    //   ________________
-    // })
+     axios.post(`http://localhost:3002/post/${postId}/comment`, { newComment }).then((res) => {
+       setComments((curr) => [...curr, res.comments]);
+       setNewComment('');
+       window.location.reload();
+     })
   }
 
   return (
@@ -27,7 +28,7 @@ const Comments = ({ postId, comments: initialComments }) => {
       <div>
         <input value={newComment} onChange={e => setNewComment(e.target.value)}/>
       </div>
-      <button  onClick={handleSubmitComment}>
+      <button onClick={handleSubmitComment}>
         Submit
       </button>
     </div>
